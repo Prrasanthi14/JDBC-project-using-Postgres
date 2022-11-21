b8b69e87652311012eedfc543900d7646bbcb2fb
Prrasanthi V S <prrasanthi.22@mountblue.tech>
2022-11-21 16:58:22 +0530

    after adding gitignore

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,21 @@ import java.sql.DriverManager;
 
 public class Main {
 
-    static String url="jdbc:postgresql://localhost:5432/iplproject";
 
     public static void main(String[] args) throws SQLException {
-        getMatchesPLayedPerYear();
-        getTotalMatchesPlayedByTeams();
-        getExtraRunsScoredIn2016();
-        getTopEconomicalBowler2015();
-    }
-
-    static void getMatchesPLayedPerYear() throws SQLException {
+        String url="jdbc:postgresql://localhost:5432/iplproject";
         Connection connection=DriverManager.getConnection(url,"prrasanv","prrasanv");
         Statement statement=connection.createStatement();
+        getMatchesPLayedPerYear(connection,statement);
+        getTotalMatchesPlayedByTeams(connection,statement);
+        getExtraRunsScoredIn2016(connection,statement);
+        getTopEconomicalBowler2015(connection,statement);
+
+        statement.close();
+        connection.close();
+    }
+
+    static void getMatchesPLayedPerYear(Connection connection,Statement statement) throws SQLException {
         ResultSet resultSet=statement.executeQuery("select season,count(*)as years_count from matches group by season;");
         ResultSetMetaData rsmd = resultSet.getMetaData();
         String name1 = rsmd.getColumnName(1);
@@ -28,13 +31,9 @@ public class Main {
             System.out.println(st1+"   "+st2);
         }
         resultSet.close();
-        statement.close();
-        connection.close();
     }
 
-    static void getTotalMatchesPlayedByTeams() throws SQLException{
-        Connection connection=DriverManager.getConnection(url,"prrasanv","prrasanv");
-        Statement statement=connection.createStatement();
+    static void getTotalMatchesPlayedByTeams(Connection connection,Statement statement) throws SQLException{
         ResultSet resultSet=statement.executeQuery("select winner as team,count(*)as no_of_matches_won from matches where winner!='' group by winner;");
         ResultSetMetaData rsmd = resultSet.getMetaData();
         String name1 = rsmd.getColumnName(1);
@@ -47,13 +46,9 @@ public class Main {
             System.out.println(st1+"   "+st2);
         }
         resultSet.close();
-        statement.close();
-        connection.close();
     }
 
-    static void getExtraRunsScoredIn2016() throws SQLException{
-        Connection connection=DriverManager.getConnection(url,"prrasanv","prrasanv");
-        Statement statement=connection.createStatement();
+    static void getExtraRunsScoredIn2016(Connection connection,Statement statement) throws SQLException{
         ResultSet resultSet=statement.executeQuery("select bowling_team as team ,sum(extra_runs)as extra_runs from deliveries join matches on deliveries.match_id = matches.id where season =  2016 group by bowling_team");
         ResultSetMetaData rsmd = resultSet.getMetaData();
         String name1 = rsmd.getColumnName(1);
@@ -66,13 +61,9 @@ public class Main {
             System.out.println(st1+"    "+st2);
         }
         resultSet.close();
-        statement.close();
-        connection.close();
     }
 
-    static void getTopEconomicalBowler2015() throws SQLException{
-        Connection connection=DriverManager.getConnection(url,"prrasanv","prrasanv");
-        Statement statement=connection.createStatement();
+    static void getTopEconomicalBowler2015(Connection connection,Statement statement) throws SQLException{
         ResultSet resultSet=statement.executeQuery("select bowler,sum(total_runs)/((count(bowler)/6) +((mod(count(bowler),6))/6)) as economy from deliveries join matches on deliveries.match_id = matches.id where matches.season =2015 group by bowler order by economy limit 10;");
         ResultSetMetaData rsmd = resultSet.getMetaData();
         String name1 = rsmd.getColumnName(1);
@@ -85,7 +76,5 @@ public class Main {
             System.out.println(st1+"   "+st2);
         }
         resultSet.close();
-        statement.close();
-        connection.close();
     }
 }
